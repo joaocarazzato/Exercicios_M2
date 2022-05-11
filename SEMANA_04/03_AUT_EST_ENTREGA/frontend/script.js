f38383aea39e195eaa4dd8b1fa7c133cec7030a2
joaocarazzato <joao.carazzato@sou.inteli.edu.br>
2022-05-11 15:19:23 -0300

    Entregavel Semana 4

---
+++ SEMANA_04/03_AUT_EST_ENTREGA/frontend/script.js
@@ -0,0 +1,8 @@
+function teste22() {
+$(ghost).append(`<br><b>Linguagens Conhecidas</b>`);
+$(peak).append(`<br><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" alt="Foto da Linguagem CSS" width="40" height="40">
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" alt="Foto da Linguagem HTML" width="40" height="40">
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/godot/godot-original.svg" alt="Foto da Linguagem Godot" width="40" height="40">
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="Foto da Linguagem JavaScript" width="40" height="40">`);
+
+}
